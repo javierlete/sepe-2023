5ec2735c47daa46abbcd2e7ecebb181c48b98d53
HTML <HTML@Isuntza05.ipartekaula.com>
2023-11-08 18:12:49 +0100

    Dao genérico para un CRUD genérico

---
+++ bases/src/com/ipartek/formacion/chat/accesodatos/Dao.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.chat.accesodatos;
+
+public interface Dao<T> {
+	public Iterable<T> obtenerTodos();
+	public T obtenerPorId(Long id);
+	
+	public T insertar(T objeto);
+	public T modificar(T objeto);
+	public void borrar(Long id);
+}
