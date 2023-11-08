f0f037cbd84b3029a5e4b6b6ecd1a8b4d8ff2898
HTML <HTML@Isuntza05.ipartekaula.com>
2023-11-08 18:13:33 +0100

    Comprobación del DaoUsuarioMemoria

---
+++ bases/src/com/ipartek/formacion/chat/pruebas/DaoUsuarioMemoriaPrueba.java
@@ -0,0 +1,29 @@
+package com.ipartek.formacion.chat.pruebas;
+
+import java.time.LocalDateTime;
+
+import com.ipartek.formacion.chat.accesodatos.DaoUsuario;
+import com.ipartek.formacion.chat.accesodatos.DaoUsuarioMemoria;
+import com.ipartek.formacion.chat.pojos.Usuario;
+
+public class DaoUsuarioMemoriaPrueba {
+
+	public static void main(String[] args) {
+		DaoUsuario dao = new DaoUsuarioMemoria();
+		
+		dao.insertar(new Usuario("Uno"));
+		dao.insertar(new Usuario("Dos"));
+		dao.insertar(new Usuario("Tres"));
+		
+		dao.modificar(new Usuario(2L, "Dos Modificado", LocalDateTime.now()));
+		
+		dao.borrar(1L);
+		
+		System.out.println(dao.obtenerPorId(2L));
+		
+		for(Usuario u: dao.obtenerTodos()) {
+			System.out.println(u);
+		}
+	}
+
+}
