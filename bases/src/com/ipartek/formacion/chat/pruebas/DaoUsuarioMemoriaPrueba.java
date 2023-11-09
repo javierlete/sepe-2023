9675136864b47b5b95bebc13f26ac53392954e6c
HTML <HTML@DESKTOP-MHK683Q.mshome.net>
2023-11-09 17:54:33 +0100

    Más pruebas

--- bases/src/com/ipartek/formacion/chat/pruebas/DaoUsuarioMemoriaPrueba.java
+++ bases/src/com/ipartek/formacion/chat/pruebas/DaoUsuarioMemoriaPrueba.java
@@ -10,26 +10,56 @@ public class DaoUsuarioMemoriaPrueba {
 
 	public static void main(String[] args) {
 		DaoUsuario dao = new DaoUsuarioMemoria();
-		
+
 		dao.insertar(new Usuario("Uno"));
 		dao.insertar(new Usuario("Dos"));
 		dao.insertar(new Usuario("Tres"));
-		
+		dao.insertar(new Usuario("Cuatro", LocalDateTime.of(2023, 1, 1, 1, 1)));
+		dao.insertar(new Usuario("Cuatro", LocalDateTime.of(2023, 3, 1, 1, 1)));
+
 		dao.modificar(new Usuario(2L, "Dos Modificado", LocalDateTime.now()));
-		
+
 		dao.borrar(1L);
-		
+
 		Usuario usuarioAModificar = dao.obtenerPorId(3L);
-		
+
+		System.out.println("Usuario id 3");
+
 		System.out.println(usuarioAModificar);
-		
+
 		usuarioAModificar.setNombre("Modificado");
-		
+
 		dao.modificar(usuarioAModificar);
+
+		System.out.println("Listado completo");
+
+		for (Usuario u : dao.obtenerTodos()) {
+			System.out.println(u);
+		}
+
+		String textoABuscar = "os";
+
+		System.out.println("Buscando " + textoABuscar);
+
+		for (Usuario u : dao.buscarPorNombre(textoABuscar)) {
+			System.out.println(u);
+		}
+
+		LocalDateTime fecha = LocalDateTime.of(2023, 5, 5, 0, 0);
+
+		System.out.println("Buscando posteriores a " + fecha);
+
+		for (Usuario u : dao.buscarPorUltimaConexion(fecha)) {
+			System.out.println(u);
+		}
+
+		LocalDateTime fechaInicio = LocalDateTime.of(2023, 2, 2, 0, 0);
+		LocalDateTime fechaFin = LocalDateTime.of(2023, 4, 4, 0, 0);
 		
-		for(Usuario u: dao.obtenerTodos()) {
+		System.out.println("Buscando posteriores a " + fechaInicio + " y anteriores a " + fechaFin);
+
+		for (Usuario u : dao.buscarPorRangoDeFechas(fechaInicio, fechaFin)) {
 			System.out.println(u);
 		}
 	}
-
 }
