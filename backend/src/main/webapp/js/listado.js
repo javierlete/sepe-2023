75fc52b310402fceede43c936907160446aa986c
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2023-11-28 16:31:48 +0100

    REST utilizado desde ficheros cliente en el servidor

---
+++ backend/src/main/webapp/js/listado.js
@@ -0,0 +1,97 @@
+'use strict';
+
+let form, table, inputId, inputNombre, inputFecha;
+
+const URL = 'http://localhost:8080/backend/api/v1/usuarios';
+
+window.addEventListener('DOMContentLoaded', async function () {
+    form = document.querySelector('form');
+    table = document.querySelector('table');
+
+    inputId = document.querySelector('#id');
+    inputNombre = document.querySelector('#nombre');
+    inputFecha = document.querySelector('#fecha');
+
+    form.style.display = 'none';
+
+    form.addEventListener('submit', guardar);
+
+    await recargarTabla();
+});
+
+async function guardar(e) {
+    e.preventDefault();
+
+    table.style.display = 'table';
+    form.style.display = 'none';
+
+    if (inputId.value) {
+		const usuario = { id: inputId.value, nombre: inputNombre.value, fechaUltimaConexion: inputFecha.value };
+		
+        await fetch(`${URL}/${inputId.value}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        });
+    } else {
+		const usuario = { nombre: inputNombre.value, fechaUltimaConexion: inputFecha.value };
+		
+        await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        });
+    }
+
+    recargarTabla();
+}
+async function recargarTabla() {
+    const respuesta = await fetch(URL);
+    const usuarios = await respuesta.json();
+
+    const tbody = document.querySelector('tbody');
+
+    tbody.innerHTML = '';
+
+    usuarios.forEach(usuario => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${usuario.id}</td>
+            <td>${usuario.nombre}</td>
+            <td>${usuario.fechaUltimaConexion}</td>
+            <td>
+                <a href="javascript:formulario(${usuario.id})">Editar</a>
+                <a href="javascript:borrar(${usuario.id})">Borrar</a>
+            </td>
+            `;
+
+        tbody.appendChild(tr);
+    });
+}
+
+async function formulario(id) {
+    table.style.display = 'none';
+    form.style.display = 'block';
+
+    if(id) {
+	    const respuesta = await fetch(`${URL}/${id}`);
+	    const usuario = await respuesta.json();
+
+        inputId.value = usuario.id;
+        inputNombre.value = usuario.nombre;
+        inputFecha.value = usuario.fechaUltimaConexion;
+    } else {
+        inputId.value = '';
+        inputNombre.value = '';
+        inputFecha.value = '';
+    }
+}
+
+async function borrar(id) {
+    await fetch(`${URL}/${id}`, { method: 'DELETE' });
+    recargarTabla();
+}
