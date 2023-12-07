0a771936198b6d79fc932a5dee64dbf3d3074084
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2023-12-07 16:54:23 +0100

    Aspecto con Bootstrap y con validaciones

--- ejemplojaxrs/src/main/webapp/js/admin.js
+++ ejemplojaxrs/src/main/webapp/js/admin.js
@@ -3,21 +3,29 @@
 const URL = 'http://localhost:8080/ejemplojaxrs/api/v2/clientes/';
 
 let inputId, inputNombre, inputApellidos, inputDni, inputTelefono, inputDireccion, inputCodigoPostal;
+let inputs;
 
 window.addEventListener('DOMContentLoaded', async function() {
+	inputs = document.querySelectorAll('input');
+
 	inputId = document.querySelector('#id');
 	inputNombre = document.querySelector('#nombre');
 	inputApellidos = document.querySelector('#apellidos');
 	inputDni = document.querySelector('#dni');
 	inputTelefono = document.querySelector('#telefono');
 	inputDireccion = document.querySelector('#direccion');
-	inputCodigoPostal = document.querySelector('#codigo-postal');
+	inputCodigoPostal = document.querySelector('#codigoPostal');
 
 	const form = document.querySelector('form');
-	
+
 	form.addEventListener('submit', async function(e) {
 		e.preventDefault();
-		
+		let input;
+
+		for (input of inputs) {
+			input.classList.remove('is-invalid');
+		}
+
 		const cliente = {
 			id: +inputId.value,
 			nombre: inputNombre.value,
@@ -25,19 +33,19 @@ window.addEventListener('DOMContentLoaded', async function() {
 			dni: inputDni.value,
 			telefono: inputTelefono.value,
 			direccion: inputDireccion.value,
-			codigoPostal : inputCodigoPostal.value
+			codigoPostal: inputCodigoPostal.value
 		};
-		
+
 		let respuesta;
-		
-		if(cliente.id) {
+
+		if (cliente.id) {
 			// PUT
 			respuesta = await fetch(URL + cliente.id, {
 				method: 'PUT',
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(cliente)
 			});
-			
+
 			console.log(respuesta);
 		} else {
 			// POST
@@ -46,21 +54,33 @@ window.addEventListener('DOMContentLoaded', async function() {
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(cliente)
 			});
-			
+
 			console.log(respuesta);
 		}
-		
-		if(!respuesta.ok) {
+
+		if (!respuesta.ok) {
 			const errores = await respuesta.json();
-			
+
 			console.log(errores);
+
+			for (const clave in errores) {
+				input = document.querySelector('#' + clave);
+				input.classList.add('is-invalid');
+			};
+		} else {
+			for (input of inputs) {
+				form.reset();
+				
+				input.classList.remove('is-invalid');
+				input.classList.remove('is-valid');
+			}
 		}
-		
+
 		console.log(cliente);
-		
+
 		await rellenarTabla();
 	});
-	
+
 	await rellenarTabla();
 });
 
@@ -69,7 +89,7 @@ async function editar(id) {
 	if (id) {
 		const respuesta = await fetch(URL + id);
 		const cliente = await respuesta.json();
-		
+
 		console.log(cliente);
 		console.log(cliente.id);
 
@@ -88,7 +108,7 @@ async function borrar(id) {
 		const respuesta = await fetch(URL + id, { method: 'DELETE' });
 
 		console.log(respuesta);
-		
+
 		await rellenarTabla();
 	}
 }
@@ -111,8 +131,8 @@ async function rellenarTabla() {
 			<td>${c.direccion || ''}</td>
 			<td>${c.codigoPostal || ''}</td>
 			<td>
-				<a href="javascript:editar(${c.id})">Editar</a>
-				<a href="javascript:borrar(${c.id})">Borrar</a>
+				<a class="btn btn-sm btn-primary" href="javascript:editar(${c.id})">Editar</a>
+				<a class="btn btn-sm btn-danger" href="javascript:borrar(${c.id})">Borrar</a>
 			</td>
 		`;
 		tbody.appendChild(tr);
