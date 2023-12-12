b73a65c0c5da19a62b2ba9f9b76f88736c842de1
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2023-12-12 17:43:30 +0100

    Ampliación de fábrica

--- ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/global/Fabrica.java
+++ ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/global/Fabrica.java
@@ -1,12 +1,18 @@
 package com.ipartek.formacion.ejemplojaxrs.global;
 
 import com.ipartek.formacion.ejemplojaxrs.repositorios.DaoCliente;
+import com.ipartek.formacion.ejemplojaxrs.repositorios.DaoUsuario;
 import com.ipartek.formacion.ejemplojaxrs.servicios.ClienteServicio;
+import com.ipartek.formacion.ejemplojaxrs.servicios.LoginServicio;
 
 import jakarta.validation.Validator;
 
 public interface Fabrica {
 	ClienteServicio getServicioCliente();
+	LoginServicio getServicioLogin();
+	
 	DaoCliente getDaoCliente();
+	DaoUsuario getDaoUsuario();
+	
 	Validator getValidator();
 }
--- ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/global/FabricaImpl.java
+++ ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/global/FabricaImpl.java
@@ -6,16 +6,22 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
 import com.ipartek.formacion.ejemplojaxrs.repositorios.DaoCliente;
+import com.ipartek.formacion.ejemplojaxrs.repositorios.DaoUsuario;
 import com.ipartek.formacion.ejemplojaxrs.servicios.ClienteServicio;
+import com.ipartek.formacion.ejemplojaxrs.servicios.LoginServicio;
 
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 
 public class FabricaImpl implements Fabrica {
 
-	private Validator validator;
 	private DaoCliente clienteDao;
+	private DaoUsuario usuarioDao;
+	
 	private ClienteServicio clienteServicio;
+	private LoginServicio loginServicio;
+
+	private Validator validator;
 
 	public static Fabrica construir(String ruta) {
 		return new FabricaImpl(ruta);
@@ -27,7 +33,10 @@ public class FabricaImpl implements Fabrica {
 			props.load(FabricaImpl.class.getClassLoader().getResourceAsStream(ruta));
 
 			String nombreClaseServiciosCliente = props.getProperty("servicios.cliente");
+			String nombreClaseServiciosLogin = props.getProperty("servicios.login");
+			
 			String nombreClaseRepositorioCliente = props.getProperty("repositorios.dao.cliente");
+			String nombreClaseRepositorioUsuario = props.getProperty("repositorios.dao.usuario");
 
 			String driver = props.getProperty("repositorios.jdbc.driver");
 			String url = props.getProperty("repositorios.jdbc.url");
@@ -37,12 +46,17 @@ public class FabricaImpl implements Fabrica {
 			clienteDao = (DaoCliente) Class.forName(nombreClaseRepositorioCliente)
 					.getConstructor(String.class, String.class, String.class, String.class)
 					.newInstance(driver, url, user, pass);
+			usuarioDao = (DaoUsuario) Class.forName(nombreClaseRepositorioUsuario)
+					.getConstructor(String.class, String.class, String.class, String.class)
+					.newInstance(driver, url, user, pass);
 
 //		clienteServicio = new com.ipartek.formacion.ejemplojaxrs.servicios.ClienteServicioImpl();
 			Class<?> claseServiciosCliente = Class.forName(nombreClaseServiciosCliente);
 			Constructor<?> constructorServiciosCliente = claseServiciosCliente.getConstructor(DaoCliente.class);
 			clienteServicio = (ClienteServicio) constructorServiciosCliente.newInstance(clienteDao);
 
+			loginServicio = (LoginServicio) Class.forName(nombreClaseServiciosLogin).getConstructor(DaoUsuario.class).newInstance(usuarioDao);
+			
 			validator = Validation.buildDefaultValidatorFactory().getValidator();
 		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
 				| IllegalAccessException | IllegalArgumentException | InvocationTargetException | IOException
@@ -64,6 +78,16 @@ public class FabricaImpl implements Fabrica {
 	@Override
 	public Validator getValidator() {
 		return validator;
+	}
+
+	@Override
+	public DaoUsuario getDaoUsuario() {
+		return usuarioDao;
+	}
+
+	@Override
+	public LoginServicio getServicioLogin() {
+		return loginServicio;
 	}
 
 }
