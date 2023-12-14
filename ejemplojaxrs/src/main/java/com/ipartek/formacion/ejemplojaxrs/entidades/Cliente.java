eb8e7ae1bdbdc0771a3816c60b4779552f6a2ea7
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2023-12-14 20:29:36 +0100

    Enlace de cliente con el sistema de hibernate JPA

--- ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/entidades/Cliente.java
+++ ejemplojaxrs/src/main/java/com/ipartek/formacion/ejemplojaxrs/entidades/Cliente.java
@@ -2,6 +2,11 @@ package com.ipartek.formacion.ejemplojaxrs.entidades;
 
 import com.ipartek.formacion.ejemplojaxrs.bibliotecas.DniValido;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,12 +21,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+	@Id
+	@GeneratedValue
 	@Min(0)
 	private Long id;
 	
 	@NotNull
 	@DniValido
+	@Column(columnDefinition = "CHAR(9)", unique = true)
 	private String dni;
 	
 	@NotNull
@@ -37,8 +47,10 @@ public class Cliente {
 	private String direccion;
 	
 	@Pattern(regexp = "^\\d{9}$", message = "debe tener un formato de 9 dígitos")
+	@Column(unique = true)
 	private String telefono;
 	
 	@Pattern(regexp = "^\\d{5}$", message = "debe tener un formato de 5 dígitos")
+	@Column(name = "codigo_postal")
 	private String codigoPostal;
 }
