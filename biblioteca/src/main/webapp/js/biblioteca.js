97615a1aaf560c3af0be344415990592efb0d351
Javier Lete <javier@eskura21.com>
2023-12-20 17:58:53 +0100

    Maqueta casi terminada en HTML con Bootstrap y conmutación de secciones con JS

--- biblioteca/src/main/webapp/js/biblioteca.js
+++ biblioteca/src/main/webapp/js/biblioteca.js
@@ -1,7 +1,46 @@
+'use strict';
+
 $(function() {
-	new DataTable('table', {
+	mostrar('formulario');
+
+	new DataTable('#listado', {
 		language: {
 			url: 'json/es-ES.json',
 		},
 	});
+
+	$('#login form').on('submit', function(e) {
+		e.preventDefault();
+		mostrar('principal');
+	});
+
+	$('#modal-reserva').on('hidden.bs.modal', function() {
+		mostrar('principal');
+	});
 });
+
+function reservar() {
+	alerta('Reservado', 'success');
+}
+
+function devolver() {
+	alert('devolver');
+}
+
+function borrar() {
+	alert('borrar');
+}
+
+function ocultar() {
+	$('section').hide();
+	$('#alerta').hide();
+}
+
+function mostrar(id) {
+	ocultar();
+	$('#' + id).show();
+}
+
+function alerta(texto, nivel) {
+	$('#alerta').show().html(texto).addClass('alert-' + nivel);
+}
