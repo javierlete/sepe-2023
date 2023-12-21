ee91343eeaf2dd6620198beb616c9c3bebb7e148
Javier Lete <javier@eskura21.com>
2023-12-21 16:53:25 +0100

    Listado de libros con constante

--- biblioteca/src/main/webapp/js/biblioteca.js
+++ biblioteca/src/main/webapp/js/biblioteca.js
@@ -1,7 +1,26 @@
 'use strict';
 
-$(function() {
-	mostrar('formulario');
+const libros = [
+	{ id: 1, titulo: 'Yo Robot', descripcion: 'Novela de ciencia ficción con robots y leyes de la robótica.', isbn: '97884350213401', unidades: 5, genero: 'Ciencia ficción', autor: 'Isaac Asimov', prestamo: null },
+	{ id: 2, titulo: '1984', descripcion: 'Distopía sobre un régimen totalitario y vigilancia extrema.', isbn: '9780451524935', unidades: 4, genero: 'Distopía', autor: 'George Orwell', prestamo: null },
+	{ id: 3, titulo: 'Fahrenheit 451', descripcion: 'Una sociedad que prohíbe y quema libros en un futuro distópico.', isbn: '9781451673319', unidades: 6, genero: 'Ciencia ficción', autor: 'Ray Bradbury', prestamo: null },
+	{ id: 4, titulo: 'El señor de las moscas', descripcion: 'Historia de supervivencia de niños en una isla desierta.', isbn: '9780571295715', unidades: 7, genero: 'Aventura', autor: 'William Golding', prestamo: null },
+	{ id: 5, titulo: 'Orgullo y Prejuicio', descripcion: 'Clásico de la literatura romántica y crítica social.', isbn: '9780141439518', unidades: 3, genero: 'Romance', autor: 'Jane Austen', prestamo: null },
+	{ id: 6, titulo: 'Matar a un ruiseñor', descripcion: 'Novela sobre prejuicios raciales y justicia en la gran depresión.', isbn: '9780060935467', unidades: 5, genero: 'Drama', autor: 'Harper Lee', prestamo: null },
+	{ id: 7, titulo: 'La sombra del viento', descripcion: 'Intriga y misterio en la Barcelona de posguerra.', isbn: '9780143126393', unidades: 5, genero: 'Misterio', autor: 'Carlos Ruiz Zafón', prestamo: null },
+	{ id: 8, titulo: 'El nombre del viento', descripcion: 'La historia de un joven músico convertido en poderoso mago.', isbn: '9780756404741', unidades: 4, genero: 'Fantasía', autor: 'Patrick Rothfuss', prestamo: null },
+	{ id: 9, titulo: 'Los juegos del hambre', descripcion: 'Tributos adolescentes luchan en un mortal reality show.', isbn: '9780439023481', unidades: 6, genero: 'Ciencia ficción', autor: 'Suzanne Collins', prestamo: null },
+	{ id: 10, titulo: 'El principito', descripcion: 'Fábula filosófica sobre un niño que viaja por el universo.', isbn: '9780156012195', unidades: 8, genero: 'Fábula', autor: 'Antoine de Saint-Exupéry', prestamo: null },
+	{ id: 11, titulo: 'Cien años de soledad', descripcion: 'Saga familiar en el mítico pueblo de Macondo.', isbn: '9780060883287', unidades: 3, genero: 'Realismo mágico', autor: 'Gabriel García Márquez', prestamo: null },
+];
+
+const personas = [
+	{ id: 1, email: 'javier@email.net', password: 'javier', nombre: 'Javier', dni: '12345678Z', rol: 'ADMIN' },
+	{ id: 2, email: 'pepe@email.net', password: 'perez', nombre: 'Pepe', dni: '87654321X', rol: 'USUARIO' },
+];
+
+$(function () {
+	mostrar('principal');
 
 	new DataTable('#listado', {
 		language: {
@@ -9,13 +28,13 @@ $(function() {
 		},
 	});
 
-	$('#login form').on('submit', function(e) {
+	$('#login form').on('submit', function (e) {
 		e.preventDefault();
 		mostrar('principal');
 	});
 
-	$('#formulario form').on('submit', function(e) {
-		if(this.checkValidity()) {
+	$('#formulario form').on('submit', function (e) {
+		if (this.checkValidity()) {
 			alert('Aceptando datos');
 		} else {
 			e.preventDefault();
@@ -23,7 +42,7 @@ $(function() {
 		}
 	})
 
-	$('#modal-reserva').on('hidden.bs.modal', function() {
+	$('#modal-reserva').on('hidden.bs.modal', function () {
 		mostrar('principal');
 	});
 });
@@ -47,6 +66,44 @@ function ocultar() {
 
 function mostrar(id) {
 	ocultar();
+
+	switch (id) {
+		case 'principal':
+			$(libros).each(function () {
+				$('#listado-libros').append(`
+				<div class="col">
+					<div class="card mb-3">
+						<div class="row g-0">
+							<div class="col-4">
+								<img src="https://picsum.photos/500/800?${this.id}" class="img-fluid rounded-start" alt="...">
+							</div>
+							<div class="col-8">
+								<div class="card-body">
+									<h5 class="card-title">${this.titulo}</h5>
+									<ul class="list-group list-group-flush">
+										<li class="list-group-item">Autor: ${this.autor}</li>
+										<li class="list-group-item">Género: ${this.genero}</li>
+									</ul>
+									<p class="card-text">
+										<small class="text-body-secondary">${this.unidades} libros disponibles</small>
+									</p>
+									<p class="card-text">
+										<small class="text-body-secondary">${this.isbn}</small>
+									</p>
+								</div>
+							</div>
+							<div class="col-12">
+								<a class="w-100 stretched-link btn btn-primary" href="javascript:mostrar('detalle')">Ver
+									más información</a>
+							</div>
+						</div>
+					</div>
+				</div>`)
+			});
+			
+			break;
+	}
+
 	$('#' + id).show();
 }
 
