e6610faba8da6c29e47db25f2dadaa2a6d2968d3
Javier Lete <javier@eskura21.com>
2023-12-28 17:53:05 +0100

    Adaptamos nuestros REST para que se comunique con los nuevos servicios

--- biblioteca/src/main/java/com/biblioteca/rest/AnonimoRest.java
+++ biblioteca/src/main/java/com/biblioteca/rest/AnonimoRest.java
@@ -1,8 +1,8 @@
 package com.biblioteca.rest;
 
-import static com.biblioteca.mockups.Mockups.*;
-
 import com.biblioteca.entidades.Persona;
+import com.biblioteca.servicios.AnonimoServicio;
+import com.biblioteca.servicios.AnonimoServicioImpl;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
@@ -11,13 +11,16 @@ import jakarta.ws.rs.QueryParam;
 
 @Path("/anonimo")
 public class AnonimoRest {
+	
+	private static final AnonimoServicio servicio = new AnonimoServicioImpl();
+	
 	@GET
 	@Path("/login")
 	public Persona login(@QueryParam("email") String email, @QueryParam("password") String password) {
-		Persona persona = personas.get(email);
+		Persona persona = servicio.iniciarSesion(email, password);
 		
-		if(persona == null || !persona.getPassword().equals(password)) {
-			throw new NotFoundException();
+		if(persona == null) {
+			throw new NotFoundException("El usuario o la password proporcionada son incorrectos");
 		}
 		
 		return persona;
--- biblioteca/src/main/java/com/biblioteca/rest/UsuarioRest.java
+++ biblioteca/src/main/java/com/biblioteca/rest/UsuarioRest.java
@@ -1,27 +1,36 @@
 package com.biblioteca.rest;
 
-import static com.biblioteca.mockups.Mockups.*;
-
-import java.util.HashSet;
 import java.util.Set;
 
 import com.biblioteca.entidades.Libro;
+import com.biblioteca.servicios.UsuarioServicio;
+import com.biblioteca.servicios.UsuarioServicioImpl;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 
 @Path("/usuario")
 public class UsuarioRest {
+	
+	private static final UsuarioServicio servicio = new UsuarioServicioImpl();
+	
 	@GET
 	@Path("/libros")
 	public Set<Libro> obtenerLibros() {
-		return new HashSet<>(libros.values());
+		return servicio.listarLibrosBiblioteca();
 	}
 	
 	@GET
 	@Path("/libros/{id}")
 	public Libro buscarLibro(@PathParam("id") Long id) {
-		return libros.get(id);
+		Libro libro = servicio.verLibroBiblioteca(id);
+		
+		if(libro == null) {
+			throw new NotFoundException("No se ha encontrado el libro " + id);
+		}
+		
+		return libro;
 	}
 }
