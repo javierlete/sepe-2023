7f6313be9b33bbc3c480e04f21febe68341da956
Javier Lete <javier@eskura21.com>
2024-01-02 18:55:01 +0100

    Anotar las clases para persistencia

--- biblioteca/src/main/java/com/biblioteca/entidades/Libro.java
+++ biblioteca/src/main/java/com/biblioteca/entidades/Libro.java
@@ -1,16 +1,53 @@
 package com.biblioteca.entidades;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Entity
+@Table(name = "libros")
 public class Libro {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
+	@Column(length = 50, nullable = false)
+	@Size(max = 50)
 	private String titulo;
+	
+	@Lob
 	private String descripcion;
+	
+	@Column(length = 13)
+	@Pattern(regexp = "^\\d{13}$")
+	@Size(max = 13)
 	private String isbn;
+	
+	@Min(0)
 	private Integer unidades;
+	
+	@Column(length = 50)
+	@Size(max = 50)
 	private String genero;
+
+	@Column(length = 50)
+	@Size(max = 50)
 	private String autor;
+	
+	@ManyToOne
+	private Persona prestatario;
 }
--- biblioteca/src/main/java/com/biblioteca/entidades/Persona.java
+++ biblioteca/src/main/java/com/biblioteca/entidades/Persona.java
@@ -2,18 +2,49 @@ package com.biblioteca.entidades;
 
 import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Entity
+@Table(name = "personas")
 public class Persona {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Email
+	@Column(length = 50)
+	@Size(max = 50)
 	private String email;
+	
+	@Column(length = 50)
+	@Size(max = 50)
 	private String password;
+
+	@Column(length = 50)
+	@Size(max = 50)
 	private String nombre;
+	
+	@Pattern(regexp = "[XYZ\\d]\\{7}[A-Za-z]")
+	@Column(columnDefinition = "CHAR(9)")
 	private String dni;
+	
+	@Column(length = 20)
+	@Size(max = 20)
 	private String rol;
 	
+	@OneToMany(mappedBy = "prestatario")
 	private Set<Libro> libros;
 }
