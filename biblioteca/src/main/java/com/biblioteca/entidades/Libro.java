ddbeb133e73ed7ef9f7d91c3d7f8cd6c9fd5a4b2
Javier Lete <javier@eskura21.com>
2024-01-03 15:40:01 +0100

    Constructor vacío para JPA

--- biblioteca/src/main/java/com/biblioteca/entidades/Libro.java
+++ biblioteca/src/main/java/com/biblioteca/entidades/Libro.java
@@ -12,9 +12,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 @Entity
--- biblioteca/src/main/java/com/biblioteca/entidades/Persona.java
+++ biblioteca/src/main/java/com/biblioteca/entidades/Persona.java
@@ -12,9 +12,13 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 @Entity
