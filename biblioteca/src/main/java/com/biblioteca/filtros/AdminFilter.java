7cdd5447f751fb8eddef39aa91ba2c6e92bbba3f
Javier Lete <javier@eskura21.com>
2024-01-04 17:23:45 +0100

    Autorización

--- biblioteca/src/main/java/com/biblioteca/filtros/AdminFilter.java
+++ biblioteca/src/main/java/com/biblioteca/filtros/AdminFilter.java
@@ -2,6 +2,8 @@ package com.biblioteca.filtros;
 
 import java.io.IOException;
 
+import com.biblioteca.entidades.Persona;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,6 +11,9 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebFilter("/admin/*")
 public class AdminFilter extends HttpFilter implements Filter {
@@ -16,6 +21,21 @@ public class AdminFilter extends HttpFilter implements Filter {
 	private static final long serialVersionUID = 1L;
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		
+		HttpSession session = req.getSession();
+		
+		Persona usuario = (Persona) session.getAttribute("usuario");
+		
+		if(usuario == null || !usuario.getRol().equals("ADMIN")) {
+			req.setAttribute("alerta", "El usuario debe estar logueado y ser administrador para entrar en esta parte de la web");
+			req.setAttribute("nivel", "danger");
+			
+			req.getRequestDispatcher("/WEB-INF/vistas/login.jsp").forward(req, res);
+			return;
+		}
+		
 		chain.doFilter(request, response);
 	}
 }
