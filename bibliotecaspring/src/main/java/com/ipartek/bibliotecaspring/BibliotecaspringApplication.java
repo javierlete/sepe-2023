5ebc95c705ea513dad199ee37989b50194509403
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2024-01-09 16:11:37 +0100

    Inicio de bibliotecaspring

---
+++ bibliotecaspring/src/main/java/com/ipartek/bibliotecaspring/BibliotecaspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.bibliotecaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaspringApplication.class, args);
+	}
+
+}
---
+++ bibliotecaspring/src/test/java/com/ipartek/bibliotecaspring/BibliotecaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.bibliotecaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
