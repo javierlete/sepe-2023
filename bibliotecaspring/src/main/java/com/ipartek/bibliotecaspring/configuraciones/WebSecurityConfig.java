e8a8bc428828ce9a31418549e2a04553de5e99ea
Curso <Curso@DESKTOP-T6JJQ6S.ipartekaula.com>
2024-01-16 15:56:32 +0100

    Seguridad basada en base de datos

--- bibliotecaspring/src/main/java/com/ipartek/bibliotecaspring/configuraciones/WebSecurityConfig.java
+++ bibliotecaspring/src/main/java/com/ipartek/bibliotecaspring/configuraciones/WebSecurityConfig.java
@@ -1,41 +1,43 @@
 package com.ipartek.bibliotecaspring.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+	private DataSource dataSource;
+	private PasswordEncoder passwordEncoder;
+	
+	// https://bcrypt-generator.com/
+	
 	// Autenticación
-	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails javier =
-			 User.withDefaultPasswordEncoder()
-				.username("javier")
-				.password("contra")
-				.roles("ADMIN")
-				.build();
-
-		UserDetails pepe =
-				User.withDefaultPasswordEncoder()
-				.username("pepe")
-				.password("perez")
-				.roles("USER")
-				.build();
-		
-			
-
-		return new InMemoryUserDetailsManager(javier, pepe);
+	public WebSecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
+	@Bean
+	PasswordEncoder encoder() {
+		return passwordEncoder;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
 	// Autorización
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -54,4 +56,20 @@ public class WebSecurityConfig {
 
 		return http.build();
 	}
+	
+	/*
+	create table users(
+		username varchar(50) not null primary key,
+		password varchar(100) not null,
+		enabled boolean not null
+	);
+	
+	create table authorities (
+		username varchar(50) not null,
+		authority varchar(50) not null,
+		constraint fk_authorities_users foreign key(username) references users(username)
+	);
+	
+	create unique index ix_auth_username on authorities (username,authority);
+	 */
 }
